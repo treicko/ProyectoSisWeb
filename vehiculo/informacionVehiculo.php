56499430fb0610c689742fedbf982c7c39caf0e3
unknown <DenysDenylson@gmail.com>
2013-12-03 17:13:23 -0400

    estilos terminados vendedor y gerente

--- vehiculo/informacionVehiculo.php
+++ vehiculo/informacionVehiculo.php
@@ -6,9 +6,9 @@
 	mysql_select_db('autoventabd') or die("Query failed". mysql_error());
 
 	$id=$_GET['id'];
-	echo "$id";
+	
 	$sql="SELECT * from vehiculos where id=".$id;
-	echo "$sql";
+	
 	//$query = "SELECT * FROM `practica5`.`productos`";
 
 	$vehiculo_editar = mysql_query($sql, $db);
@@ -20,10 +20,13 @@
 	<h1><?php echo $fila->marca; ?></h1>
 	<h2><?php echo $fila->modelo; ?></h2>
 	<h1><?php echo $fila->preciooferta; ?> $u$</h1>
-	<div id="imagen">
+	<div id="column1">
 	<img src='<?php echo $fila->foto; ?>' width="200" height="200">
 	</div>
+
+	<div id="column2">
 	 <p><?php echo $fila->descripcion; ?></p>
+	 </div>
 </section>
 
 
